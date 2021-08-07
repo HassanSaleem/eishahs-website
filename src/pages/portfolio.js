bbc5fc53be813e173d2d9234845f0e3056224814
Hassan Saleem <hassan100@gmail.com>
2021-08-07 16:13:19 +0100

    added portfolio images

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -5,14 +5,42 @@ import {Fade, Container, Col, Row, Button, Modal} from 'react-bootstrap'
 
 function Portfolio() {
 
-  const [count, setCount] = useState(0);
+  const [show, setShow] = useState(false);
+  const [modalImage, setModalImage] = useState(0)
 
-  return     <Fade in={true} appear={true}>
+  const handleClose = () => setShow(false);
+  const handleShow = (img) => {
+    setModalImage(img)
+    setShow(true);
+    console.log(modalImage)
+  }
+  const imgLayout = [[1,2,3],[4,5],[6,7,8]]
+
+
+  return  <Fade in={true} appear={true}>
 
       <div className="portfolio">
         <div className="title">Portfolio</div>
         <br/>
-
+        <Container className="gallery">
+          {imgLayout.map(row => {
+            return <Row>
+              {row.map(col => {
+                return <Col >
+                <div className="portfolioCol" onClick={() => handleShow(col)}>
+                <img src={"images/IMG_portfolio".concat(col, "-min.jpg")}/>
+                </div>
+                </Col>
+              })}
+            </Row>
+          })}
+        </Container>
+        <Modal show={show} onHide={handleClose} dialogClassName="portfolioModal">
+          <Modal.Header closeButton />
+          <Modal.Body>
+            <img src={"images/IMG_portfolio".concat(modalImage, "-min.jpg")}/>
+          </Modal.Body>
+        </Modal>
       </div>
 
     </Fade>
